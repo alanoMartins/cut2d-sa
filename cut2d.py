509425fe164c5b7694409691ec0b925479e8bc01
Alano Martins <alanomartinspinto@gmail.com>
2017-06-11 01:29:26 -0300

    Sim_an alternative WIP

--- cut2d.py
+++ cut2d.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import math
+import random
 
 
 def gen_solution():
@@ -11,20 +12,24 @@ def cmp_solution(s0, s1):
     []
 
 
-def initial_temperature(solution, n):
-    n
-
-
 def initial_solution():
     []
 
 
-def perturba(s):
-    return gen_solution()
+def perturba(temperatures, actual_index, interator):
+    next_idx = random.choice(xrange(0, len(temperatures)))
+    diff = temperatures[next_idx] - temperatures[actual_index]
+
+    if diff < 0:
+        return next_idx
+    else:
+        p = math.exp(-diff / interator)
+        return next_idx if random.random() < p else actual_index  # Permuta ??
 
 
 def f(s):
     s
+
 
 s0 = gen_solution()
 
@@ -32,6 +37,7 @@ M = 5000
 P = 1000
 L = 10000
 a = 0.8
+
 
 # print setup
 
@@ -41,13 +47,14 @@ def sa():
     t = initial_temperature()
     j = 1
     nsucc = 0
+
     while True:
         i = 1
         while True:
             si = perturba(s)
             diff_s = f(si) - f(s)
 
-            if diff_s <= 0 or math.exp(-diff_s/t) > math.random(0, 1):
+            if diff_s <= 0 or math.exp(-diff_s / t) > math.random(0, 1):
                 s = si
                 nsucc = nsucc + 1
             i = i + 1
@@ -61,4 +68,42 @@ def sa():
         if nsucc == 0 or j >= M:  # stop condition
             break
 
-    # print best solution
+            # print best solution
+
+
+def initial_temperature(size):
+    return [random.randrange(size) for p in range(0, size)]
+
+
+def sa_alt():
+    temperatures = initial_temperature(L)
+    initial_index = random.choice(xrange(0, len(temperatures)))  # Ele deu uma dica de como escolher o primeiro
+    indice_local = initial_index
+    indice_melhor = initial_index
+    interator = 1
+    alpha = 0.8
+    LIMIT = 1000
+    LIMIT_REPEAT = 50
+    counter_breaker = 0
+
+    while interator < LIMIT:
+
+        indice_local_aux = perturba(temperatures, indice_local, interator)
+
+        if indice_local == indice_local_aux:
+            counter_breaker += 1
+        else:
+            indice_local = indice_local_aux
+            counter_breaker = 0
+
+        if counter_breaker > LIMIT_REPEAT:
+            break
+
+        if temperatures[indice_local] < temperatures[indice_melhor]:
+            indice_melhor = indice_local
+        interator = + 1
+
+    print "Minimum temp found: %d at index: %d" % (temperatures[indice_melhor], indice_melhor)
+
+
+sa_alt()
